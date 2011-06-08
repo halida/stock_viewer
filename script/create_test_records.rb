180213f9fc5542eee1e767eee5d1889d8704d024
linjunhalida <linjunhalida@gmail.com>
2011-06-08 14:28:54 +0800

    more data?

--- script/create_test_records.rb
+++ script/create_test_records.rb
@@ -16,14 +16,15 @@ def create_tests
   [['GOOL', 100.0], ['MS', 50.0], ['FB', 80.0]].each do |name, price|
     stock = Stock.create!(:name => name)
     p = price
-    
-    (1..30).each do |i|
+
+    startt = Time.new(2001, 1, 1).to_i
+    (1..100).each do |i|
       next_p = p + rand_p(price)
       h = p + price/20.0 + rand_p(price)
       l = p - price/20.0 - rand_p(price)
       stock.records.create!(
                             :interval => 'minute',
-                            :start => DateTime.new(2011, 2, 3, 10, i),
+                            :start => Time.at(startt + 60 * i),
                             :o => randrange(l, h),
                             :c => randrange(l, h),
                             :h => h,
