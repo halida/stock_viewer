ce2ce197eccf662f67293261b7ca54fb2c4ff7ac
linjunhalida <linjunhalida@gmail.com>
2011-06-08 11:38:44 +0800

    change to jquery

--- public/javascripts/stock_display.js
+++ public/javascripts/stock_display.js
@@ -1,3 +1,7 @@
 function create_stock_display(name, data){
-    //todo
+    var canvas = $(name)[0];
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = '#e2f0a1';
+    //ctx.fill('#000000');
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
